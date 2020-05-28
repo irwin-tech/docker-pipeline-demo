84ec26d2f9771eb8822c1007ff780ace8d54be62
Shayakh Ahmed Rezoan <r4redu@gmail.com>
2020-05-28 14:03:52 +0600

    Update dev-php-demo-ecs-deploy.sh

--- dev-php-demo-ecs-deploy.sh
+++ dev-php-demo-ecs-deploy.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-
-#Script to get current task definition, and based on that add new ecr image address to old template and remove attributes that are not needed, then we send new task definition, get new revision number from output and update service
-set -e
 ECR_IMAGE="700707367057.dkr.ecr.us-east-1.amazonaws.com/$1:$2"
+echo "$ECR_IMAGE"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition dev-$1 --region us-east-1)
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_TASK_INFO=$(aws ecs register-task-definition --region us-east-1 --cli-input-json "$NEW_TASK_DEFINTIION")
